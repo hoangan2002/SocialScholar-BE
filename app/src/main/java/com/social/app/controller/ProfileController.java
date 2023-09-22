23f0c9a79e6bc385ff944f09d0377dfce207b216
doitdayone <dorayaki22t52gmail.com>
2023-09-22 08:34:06 +0700

    chinh sua lai edit profile

--- app/src/main/java/com/social/app/controller/ProfileController.java
+++ app/src/main/java/com/social/app/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package com.social.app.controller;
 
 import com.social.app.model.User;
+import com.social.app.repository.UserRepository;
 import com.social.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,9 @@ public class ProfileController {
     @Autowired
     private UserService theUserService;
 
+    @Autowired
+    private UserRepository theUserRepository;
+
     @GetMapping("/{userId}")
     public User getUserProfile(@PathVariable int userId) {
         return theUserService.findById(userId);
@@ -18,13 +22,10 @@ public class ProfileController {
 
     @PutMapping("/edit")
     public User editUserProfile(@RequestBody User theUser){
-        User dbUserId = theUserService.findById(theUser.getUserId());
-        String theUserName = theUser.getUserName();
-        if(!dbUserId.getUserName().equals(theUserName)){
-            if(theUserService.existUserName(theUserName))
-                throw new RuntimeException("The Username was existed");
-            else return theUserService.save(theUser);
-        }
-        else return theUserService.save(theUser);
+        return theUserRepository.findById(theUser.getUserId()).map(user -> {
+            user.setUserName(theUser.getUserName());
+            user.setPhone(theUser.getPhone());
+            return theUserRepository.save(user);
+        }).orElseThrow(()-> new RuntimeException("Can not edit this profile - "+ theUser.getUserId()));
     }
 }
