bef0cc45420ee213cb627f827cbe433448eb293c
doitdayone <dorayaki22t52gmail.com>
2023-09-25 12:44:06 +0700

    gop add point va level up

--- app/src/main/java/com/social/app/service/UserService.java
+++ app/src/main/java/com/social/app/service/UserService.java
@@ -1,5 +1,6 @@
 package com.social.app.service;
 import com.social.app.model.MemberActivity;
+import com.social.app.model.MemberType;
 import com.social.app.model.Role;
 import com.social.app.model.User;
 import com.social.app.repository.UserRepository;
@@ -105,8 +106,10 @@ public class UserService implements UserDetailsService {
     }
 
     // Cong diem cho user, goi ham moi khi thuc hien hanh dong
-    public void addPoints(User user, Enum<MemberActivity> activity){
+    public User addPoints(User user, Enum<MemberActivity> activity){
         int activityPoint = user.getActivityPoint();
+        String originLevel = user.getLevel();
+        // set point
         if(activity.equals(MemberActivity.COMMENT_VOTED))
             user.setActivityPoint(activityPoint+2);
         else if (activity.equals(MemberActivity.DONATE)||activity.equals(MemberActivity.VOTE)||activity.equals(MemberActivity.BUY_DOCUMENT)||activity.equals(MemberActivity.POST_VOTED))
@@ -116,5 +119,30 @@ public class UserService implements UserDetailsService {
         else if (activity.equals(MemberActivity.POST))
             user.setActivityPoint(activityPoint+20);
         else throw new RuntimeException("Invalid activity");
+
+        // activity point sau khi set
+        activityPoint = user.getActivityPoint();
+
+        // setLevel
+        if (activityPoint<500)
+            user.setLevel(String.valueOf(MemberType.BEGINNER));
+        else if (activityPoint<2000)
+            user.setLevel(String.valueOf(MemberType.ACTIVE));
+        else
+            user.setLevel(String.valueOf(MemberType.EXPERT));
+
+        // so sanh level trc sau va hien thbao
+        String updateLevel = user.getLevel();
+        if (!originLevel.equals(updateLevel)){
+            System.out.println("CONGRATULATION, YOUR LEVEL IS UPGRADED TO: "+ updateLevel);
+        }
+
+        // luu vao database
+        return repository.findById(user.getUserId())
+                .map(userDb -> {
+                    userDb.setActivityPoint(user.getActivityPoint());
+                    userDb.setLevel(updateLevel);
+                    return repository.save(userDb);
+                } ).orElseThrow(()-> new RuntimeException("Fail!"));
     }
 }
